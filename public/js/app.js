bc93edab0ae9a2d45b71692e3e5f6ecea19d6144
HOME <home@HOMEs-MacBook-Pro.local>
2022-03-16 17:46:38 +0300

    new files added

---
+++ public/js/app.js
@@ -0,0 +1,124 @@
+function checkUserFullName(){
+    var userSurname = document.getElementById("userFullName").value;
+    var flag = false;
+    if(userSurname === ""){
+        flag = true;
+    }
+    if(flag){
+        document.getElementById("userFullNameError").style.display = "block";
+    }else{
+        document.getElementById("userFullNameError").style.display = "none";
+    }
+}
+// xxxxxxxxxx User Surname Validation xxxxxxxxxx
+function checkUserSurname(){
+    var userSurname = document.getElementById("userSurname").value;
+    var flag = false;
+    if(userSurname === ""){
+        flag = true;
+    }
+    if(flag){
+        document.getElementById("userSurnameError").style.display = "block";
+    }else{
+        document.getElementById("userSurnameError").style.display = "none";
+    }
+}
+// xxxxxxxxxx Email Validation xxxxxxxxxx
+function checkUserEmail(){
+    var userEmail = document.getElementById("userEmail");
+    var userEmailFormate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var flag;
+    if(userEmail.value.match(userEmailFormate)){
+        flag = false;
+    }else{
+        flag = true;
+    }
+    if(flag){
+        document.getElementById("userEmailError").style.display = "block";
+    }else{
+        document.getElementById("userEmailError").style.display = "none";
+    }
+}
+// xxxxxxxxxx Password Validation xxxxxxxxxx
+function checkUserPassword(){
+    var userPassword = document.getElementById("userPassword");
+    var userPasswordFormate = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}/;      
+    var flag;
+    if(userPassword.value.match(userPasswordFormate)){
+        flag = false;
+    }else{
+        flag = true;
+    }    
+    if(flag){
+        document.getElementById("userPasswordError").style.display = "block";
+    }else{
+        document.getElementById("userPasswordError").style.display = "none";
+    }
+}
+function signUp(){
+    var userFullName = document.getElementById("userFullName").value;
+    var userSurname = document.getElementById("userSurname").value;
+    var userEmail = document.getElementById("userEmail").value;
+    var userPassword = document.getElementById("userPassword").value;
+    var userFullNameFormate = /^([A-Za-z.\s_-])/;    
+    var userEmailFormate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var userPasswordFormate = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}/;      
+
+    var checkUserFullNameValid = userFullName.match(userFullNameFormate);
+    var checkUserEmailValid = userEmail.match(userEmailFormate);
+    var checkUserPasswordValid = userPassword.match(userPasswordFormate);
+
+    if(checkUserFullNameValid == null){
+        return checkUserFullName();
+    }else if(userSurname === ""){
+        return checkUserSurname();
+    }else if(checkUserEmailValid == null){
+        return checkUserEmail();
+    }else if(checkUserPasswordValid == null){
+        return checkUserPassword();
+    }else{
+
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth,userEmail, userPassword).then((userCredential) => {
+           // var user = firebase.auth().currentUser;
+            const user = userCredential.user;
+
+            var uid;
+            if (user != null) {
+                uid = user.uid;
+            }
+            var userData = {
+                userFullName: userFullName,
+                userSurname: userSurname,
+                userEmail: userEmail,
+                userPassword: userPassword,
+                
+            }
+
+            const database = getDatabase();
+
+            set(ref(database, 'Users/' + 1),userData);
+
+
+           // var firebaseRef = database.ref();
+            
+            //firebaseRef.child(uid).set(userData);
+            // swal('Your Account Created','Your account was created successfully, you can log in now.',
+            // ).then((value) => {
+            //     setTimeout(function(){
+            //         window.location.replace("../index.html");
+            //     }, 1000)
+            // });
+        }).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorMessage)
+            // swal({
+            //     type: 'error',
+            //     title: 'Error',
+            //     text: "Error",
+            // })
+        });
+    }
+}
